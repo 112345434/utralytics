b784d24625ebc2ca32fd5572c29dbe31dd4a1a14
112345434 <2059702403@qq.com>
2024-04-14 17:57:19 +0800

    First commit

---
+++ fabric/README.dataset.txt
@@ -0,0 +1,6 @@
+# fabric defect detection > 2023-03-24 9:52pm
+https://universe.roboflow.com/rasslen-workspace/fabric-defect-detection-ncx3q
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
---
+++ train2.py
@@ -0,0 +1,21 @@
+import warnings
+warnings.filterwarnings('ignore')
+from ultralytics import YOLO
+
+if __name__ == '__main__':
+    model = YOLO('C:\\Users\\jsj_gjj\\Desktop\\utralytics-main\\ultralytics\\cfg\\models\\v8\\yolov8_RepGhost.yaml')
+    # model.load('yolov8n.pt') # loading pretrain weights
+    model.train(data='C:\\Users\\jsj_gjj\\Desktop\\utralytics-main\\AITEX.yaml',
+
+                imgsz=640,
+                epochs=300,
+                batch=32,
+
+                device='1',
+                # using SGD
+                # resume='', # last.pt path
+                # amp=False, # close amp
+                # fraction=0.2,
+                project='C:\\Users\\jsj_gjj\\Desktop\\ultralytics-main\\runs\\detect',
+                name='exp',
+                )
